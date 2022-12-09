90f336d873ef971840ac465621cdfd4af37594ef
Techiegold23 <TechieGold23>
2022-12-09 08:59:44 -0800

    This programs prints a line of text using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,12 @@
-/**
- * file: 5-printf.c
- * Auth: Gold Israel
- */
-
 #include<stdio.h>
 
 /**
- * main - prints exactly with proper grammar, but the outcome is a piece of art,
- *             followed by a new line.
- *
- * return: Always 0
-*/
+ * main - Entry point
+ * Return: 0
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");	
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
