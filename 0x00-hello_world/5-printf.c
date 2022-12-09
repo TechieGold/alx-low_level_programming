ce7f9195439dfe88c8b92368a67ac190251baa86
Techiegold23 <TechieGold23>
2022-12-08 19:56:10 -0800

    THis program prints a text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,14 @@
  */
 
 #include <stdio.h>
+
+/**
+ * main - prints exactly with proper grammar, but the outcome is a piece of art,
+ *             followed by a new line.
+ *
+ * return: Always 0
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art");	
