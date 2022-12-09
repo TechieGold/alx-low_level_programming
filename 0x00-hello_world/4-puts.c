efd10fabc3dddd013bd7a6fc400b14dabce0b12f
Techiegold23 <TechieGold23>
2022-12-08 20:13:25 -0800

    This program prints a text using the printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,15 @@
 
 #include<stdio.h>
 
+/**
+ *main: Write a C program that prints exactly 
+ * "Programming is like building a multilingual puzzle
+ * Followed by a new line
+ * return: 0
+ */
 int main(void) 
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	
 	return 0;
 }	
