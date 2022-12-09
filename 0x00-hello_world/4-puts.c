97bf93eb1e17cabb1945c25623fdf5baeeec8330
Techiegold23 <TechieGold23>
2022-12-09 08:38:13 -0800

    This program prints text using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,15 @@
-/**
- * File: 4-puts.c
- * Auth : Gold Israel
- *
-*/
-
 #include <stdio.h>
 
 /**
- * main  - prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * main - prints exactly "Programming is like building a
+ * multilingual puzzle, followed by a new line.
  *
  * Return: Always 0
 */
 
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
