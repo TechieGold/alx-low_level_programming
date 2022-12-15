0aeea4156688c6d3f3490e2bf975246408fca0d6
Techiegold23 <TechieGold23>
2022-12-14 20:00:39 -0800

    Absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _abs - Entry point
+  * @int:
+  * Description - Write a function that computes the absolute
+  * value of an integer
+  * Return: Always 0
+  */
+
+int _abs(int r)
+{
+	if (r > 1)
+		return (r);
+	else
+		return (-r);
+
+}
