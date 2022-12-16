1ebc76931bd713e83132b09cccb2922e93ffed5f
Techiegold23 <TechieGold23>
2022-12-15 20:13:43 -0800

    ‘variables’

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a >= b && b >= c)
-{
-	largest = a;
-}
-else if (b >= a && a >= c)
-{
-	largest = b;
-}
-else
-{
-	largest = c;
-}
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-return (largest);
+	return (largest);
 }
