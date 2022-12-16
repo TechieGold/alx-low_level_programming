25ed1bab2428c328ba3fb4257fb18d5b6602b1cd
Techiegold23 <TechieGold23>
2022-12-16 02:06:00 -0800

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,10 +27,10 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-	}
-	else
-	{
+		else
+		{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
+		}
 	}
 }
