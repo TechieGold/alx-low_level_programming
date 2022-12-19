0de99a7a47925818883ae3044804122f2072e8b4
Techiegold23 <TechieGold23>
2022-12-18 17:58:37 -0800

    This function adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Description - write a function that adds two integers
+ * and return the result
+ * Return: result
+ */
+
+int add(int a,  int b)
+{
+	return (a + b);
+}
