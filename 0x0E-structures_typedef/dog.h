7b1a757fce1258679cca8636a9e7df7cccea03d0
Techiegold23 <TechieGold23>
2023-01-17 13:55:39 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+#endif
+
+/**
+  * struct dog - structure for a basic dog
+  * @name: Name of dog
+  * @age: Age of dog
+  * @owner: Owner of dog
+  *
+  * Description: Data structure for dog
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
