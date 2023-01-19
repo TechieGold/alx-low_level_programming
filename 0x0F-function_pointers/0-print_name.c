dfb7b4735b6ffd6669eed3bfe28148d7259761e3
Techiegold23 <TechieGold23>
2023-01-18 19:24:25 -0800

    Added files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,7 +9,7 @@
   * Return: 0 on success.
   */
 
-void print_name(char *name, void (*f)(char*))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 void print_name(char *name, void(*f)(char *));
 #endif
