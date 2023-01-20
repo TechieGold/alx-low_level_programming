75978c74cd990af4e18096c6d834d041a3c2bd99
Techiegold23 <TechieGold23>
2023-01-20 02:13:33 -0800

    Added files

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTION_H_
+#define VARIADIC_FUNCTION_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* variadic function - ALX*/
