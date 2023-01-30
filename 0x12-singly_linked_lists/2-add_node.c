535a6e3f8bfcd727074a2c3d7e56f2fb68d35c24
Gold Israel <techiegold101@gmail.com>
2023-01-30 15:02:07 -0800

    updated files

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,22 @@
   */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node = malloc(sizeof(list_t));
-    if (!new_node)
-        return NULL;
+	list_t *new_node = malloc(sizeof(list_t));
 
-    new_node->str = strdup(str);
-    if (!new_node->str)
-    {
-        free(new_node);
-        return NULL;
-    }
+	if (!new_node)
+		return (NULL);
 
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
+	new_node->str = strdup(str);
 
-    return new_node;
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
