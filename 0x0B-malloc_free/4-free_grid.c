e9079a277b72918c53b2b72741f80e9907ff1538
Gold Israel <techiegold101@gmail.com>
2023-06-14 19:39:38 -0700

    Implement betty code syle

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,15 +8,14 @@
  * Return: void.
  */
 
- void free_grid(int **grid, int height)
- {
- 	int i;
+void free_grid(int **grid, int height)
+{
+	int i;
 
- 	if (grid == NULL)
+	if (grid == NULL)
 		return;
 
 	for (i = 0; i < height; i++)
 		free(grid[i]);
-	
 	free(grid);
 }
