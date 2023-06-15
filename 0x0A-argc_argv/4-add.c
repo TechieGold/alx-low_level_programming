9b3a1da8696f7318d1c2e93b13ff156c320368e1
Gold Israel <techiegold101@gmail.com>
2023-06-14 17:44:04 -0700

    Add program that adds only positive numbers in cmd line arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - A program that adds positive numbers.
+ * @argc: Arguments' couter.
+ * @argv: Argument's values.
+ *
+ * Return: 0 0n success.
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j, number, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		number = atoi(argv[i]);
+		sum += number;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
