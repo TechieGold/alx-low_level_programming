a7459a6cd763ebea9ec21422320a4c01cbde5d4b
Gold Israel <techiegold101@gmail.com>
2023-06-17 03:00:13 -0700

    Calculate total number of bytes needed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	byte_access = (char *)ptr;
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		byte_access[i] = 0;
 
 	return (ptr);
