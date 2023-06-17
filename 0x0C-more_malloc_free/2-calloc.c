1e4fc57591da0065c162b709d51d95e6b5f64e49
Gold Israel <techiegold101@gmail.com>
2023-06-17 02:45:30 -0700

    Add implementation of _calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory for an array.
+ * @nmemb: Number of element.
+ * @size: Size of each element in bytes.
+ *
+ * Return: pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr = NULL;
+	unsigned int i = 0;
+	char *byte_access = NULL;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	byte_access = (char *)ptr;
+
+	for (i = 0; i < nmemb; i++)
+		byte_access[i] = 0;
+
+	return (ptr);
+}
