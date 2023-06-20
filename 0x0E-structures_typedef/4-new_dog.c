4775b2d8220cb0e4edcf0dc6d437d1a3bfba1ff1
Gold Israel <techiegold101@gmail.com>
2023-06-19 20:33:03 -0700

    Impement my own strcpy and strlen function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
-#include <string.h>
 #include "dog.h"
+
+char *my_strcpy(char *dest, const char *src);
+unsigned int my_strlen(const char *str);
 
 /**
  * new_dog - function that creates a new dog.
@@ -16,16 +18,63 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (newDog == NULL)
 		return (NULL);
 
-	newDog->name = (char *)malloc(strlen(name) + 1);
-	newDog->owner = (char *)malloc(strlen(owner) + 1);
+	newDog->name = (char *)malloc(my_strlen(name) + 1);
+	newDog->owner = (char *)malloc(my_strlen(owner) + 1);
 
 	if (newDog->name == NULL || newDog->owner == NULL)
 		return (NULL);
 
-	strcpy(newDog->name, name);
-	strcpy(newDog->owner, owner);
+	my_strcpy(newDog->name, name);
+	my_strcpy(newDog->owner, owner);
 
 	newDog->age = age;
 
 	return (newDog);
+}
+
+
+/**
+ * my_strcpy - Copies a string to a varaible.
+ * @dest: Destination.
+ * @src: Source.
+ *
+ * Return: A pointer to the destination string.
+ */
+char *my_strcpy(char *dest, const char *src)
+{
+	char *dest_ptr = dest;
+	const char *src_ptr = src;
+
+	if (dest == NULL || src == NULL)
+		return (NULL);
+	while (*src_ptr != '\0')
+	{
+		*dest_ptr = *src_ptr;
+		dest_ptr++;
+		src_ptr++;
+	}
+	*dest_ptr = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * my_strlen - Finds the length of a string.
+ * @str: The string.
+ *
+ * Return: Length of string.
+ */
+unsigned int my_strlen(const char *str)
+{
+	unsigned int length = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while (str[length] != 0)
+		length++;
+
+	return (length);
+
 }
