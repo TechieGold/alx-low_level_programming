e7c3cf970a9767a55fbcabe37593c5f1dfd44e53
Gold Israel <techiegold101@gmail.com>
2023-06-19 19:18:42 -0700

    Define a new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Description: A data strcuture that defines a dogs name, ages and owner.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
