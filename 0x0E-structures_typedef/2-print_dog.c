1654aa095db5bbc1c36b9ac70db9d867fa22e7ca
Gold Israel <techiegold101@gmail.com>
2023-06-20 20:16:22 -0700

    Add line break in line 19

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,12 +16,12 @@ void print_dog(struct dog *d)
 	if (d->name != NULL)
 		printf("Name: %s\n", d->name);
 	else
-		printf("Name: (nill)");
+		printf("Name: (nill)\n");
 	printf("Age: %f\n", d->age);
 
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
 	else
-		printf("owner: (nill)\n");
+		printf("Owner: (nill)\n");
 
 }
