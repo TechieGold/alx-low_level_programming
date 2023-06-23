f8b896fb9360f2eca798ca28dc2e89e55f8d0730
Gold Israel <techiegold101@gmail.com>
2023-06-23 02:30:16 -0700

    Add array_iterator prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,6 @@
 #include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS */
