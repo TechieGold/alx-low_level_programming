a0a3abb702303044bde18f118676834c4e83c6eb
Gold Israel <techiegold101@gmail.com>
2023-06-23 04:13:49 -0700

    Remove line that check is array or cmp is NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,8 +14,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size < 0)
 		return (-1);
-	if (array == NULL || cmp == NULL)
-		return (0);
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]) != 0)
 			return (i);
