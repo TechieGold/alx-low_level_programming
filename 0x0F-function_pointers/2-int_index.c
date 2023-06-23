9084d3fee305932d890d0bdca0ed440bb939db60
Gold Israel <techiegold101@gmail.com>
2023-06-23 12:44:04 -0700

    Check is array or cmp is NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,8 +12,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 0)
+	if (array == 0 || cmp == 0 || size <= 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]) != 0)
 			return (i);
