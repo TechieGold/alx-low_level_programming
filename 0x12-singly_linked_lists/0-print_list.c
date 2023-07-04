e9895fd0ac86d0d81262184fdc8e104513c72b93
Gold Israel <techigold101@gmail.com>
2023-07-04 08:09:47 -0700

    Add function that print a linked list and return the length

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * print_list - prints all elements of a list and return the number of nodes.
+  * @h: pointer to the first element in the linked list.
+  *
+  * Return: Number of nodes in the linked list.
+  */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%lu] %s\n", strlen(h->str),  h->str);
+		else
+			printf("[0] (nill)\n");
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
