c09b2be26628b408e15213c45c8fbbdf79d9350b
TechieGold <techiegold101@gmail.com>
2023-12-11 14:39:38 +0100

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
 
+#include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif /* SEARCH_ALGOS */
