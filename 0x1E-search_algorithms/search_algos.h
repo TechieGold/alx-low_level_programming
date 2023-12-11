cc94402b647623b37ded3a8a394b6f58ba5084ab
TechieGold <techiegold101@gmail.com>
2023-12-11 14:37:33 +0100

    add header file with prototypes

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif /* SEARCH_ALGOS */
