e425cabf5e1cf6079e39e8757f98c1fa50f39c44
TechieGold <techiegold101@gmail.com>
2023-12-11 11:40:40 +0100

    add func that searches for a value using linearly

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches for a value in an array of integers.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in array.
+ * @value: Value to search for.
+ *
+ * Return: index where the value is located or -1 if the value is not found.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
