34bb8b9c60ac56dbcf46f325b29dd60028fdb036
TechieGold <techiegold101@gmail.com>
2023-12-12 15:14:48 +0100

    change code to compile usinf -std=89

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,9 +10,11 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i = 0;
+
 	if (array == NULL)
 		return (-1);
-	for (size_t i = 0; i < size; i++)
+	for (; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
