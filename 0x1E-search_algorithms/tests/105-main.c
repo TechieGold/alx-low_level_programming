ce5eca92f27ae3461e1c902e465a9f84475fa174
TechieGold <techiegold101@gmail.com>
2023-12-13 16:53:32 +0100

    more test files

---
+++ 0x1E-search_algorithms/tests/105-main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../search_algos.h"
+
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+    listint_t *list, *res;
+    int array[] = {
+        0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 53, 61, 62, 76, 99
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    list = create_list(array, size);
+    print_list(list);
+
+    res =  jump_list(list, size, 53);
+    printf("Found %d at index: %lu\n\n", 53, res->index);
+    res =  jump_list(list, size, 2);
+    printf("Found %d at index: %lu\n\n", 2, res->index);
+    res =  jump_list(list, size, 999);
+    printf("Found %d at index: %p\n", 999, (void *) res);
+
+    free_list(list);
+    return (EXIT_SUCCESS);
+}
+
+/**
+ * create_list - Creates a single linked list
+ *
+ * @array: Pointer to the array to use to fill the list
+ * @size: Size of the array
+ *
+ * Return: A pointer to the head of the created list (NULL on failure)
+ */
+listint_t *create_list(int *array, size_t size)
+{
+	listint_t *list;
+	listint_t *node;
+
+	list = NULL;
+	while (array && size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+		{
+			free_list(list);
+			return (NULL);
+		}
+		node->n = array[size];
+		node->index = size;
+		node->next = list;
+		list = node;
+	}
+	return (list);
+}
+
+/**
+ * free_list - Deallocates a singly linked list
+ *
+ * @list: Pointer to the linked list to be freed
+ */
+void free_list(listint_t *list)
+{
+	listint_t *node;
+
+	if (list)
+	{
+		node = list->next;
+		free(list);
+		free_list(node);
+	}
+}
+
+/**
+ * print_list - Prints the content of a listint_t
+ *
+ * @list: Pointer to the head of the list
+ */
+void print_list(const listint_t *list)
+{
+	printf("List :\n");
+	while (list)
+	{
+		printf("Index[%lu] = [%d]\n", list->index, list->n);
+		list = list->next;
+	}
+	printf("\n");
+}
