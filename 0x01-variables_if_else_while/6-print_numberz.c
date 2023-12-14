5965763619e83c9d318e78cc135d609d119956d7
TechieGold <techiegold101@gmail.com>
2023-12-14 17:47:49 +0100

    modify and improve code

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,20 +2,24 @@
 #include <stdlib.h>
 
 /**
-  * main - Entry point
-  * Return: Always 0
-  */
+ * main - Entry point
+ * Description: Use `putchar` to print all number of base 10 starting from 0.
+ * Use putchar only twice.
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
-	int num = '0';
+	int i = '0';
 
-	while (num <= '9')
+	while (i <= '9')
 	{
-		putchar(num);
-		num++;
+		putchar(i);
+		i++;
 	}
 	putchar('\n');
+
 	return (0);
 
 }
